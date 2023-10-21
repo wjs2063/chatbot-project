923dd91f85b9216d6922ccf7d87fc2033cfb8e68
jaehyeon <wjs2063@naver.com>
2023-10-21 20:04:10 +0900

    fix : CORS origin  추가

--- backend/server/app/main.py
+++ backend/server/app/main.py
@@ -2,18 +2,21 @@ from fastapi import FastAPI
 from api.api_v1.api import api_router
 from core.config import settings
 import sqlalchemy
-from db.session import get_db,engine
+from db.session import get_db, engine
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from asyncio import run
 from db.session import *
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
-app.include_router(api_router,prefix = settings.API_V1_STR)
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 origins = [
     "http://localhost:3000",
-    #"http://172.30.1.50:3000"
+    # "http://172.30.1.50:3000",
+    "http://www.codeplanet.site",
+    "http://www.codeplanet.site:50001",
 ]
 
 app.add_middleware(
@@ -24,19 +27,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.on_event("startup")
 async def startup():
     print("fastapi-server started!!")
-    await create_tables(engine = engine,metadata = metadata)
+    await create_tables(engine=engine, metadata=metadata)
     print("postgresql-tables created!!")
+
+
 @app.on_event("shutdown")
 async def shutdown():
     print("fastapi-server closed!!")
 
+
 @app.get("/")
 async def test():
-    return {"DB_USER" : settings.DB_USER,
-            "DB_PASSWORD" : settings.DB_PASSWORD
+    return {"DB_USER": settings.DB_USER,
+            "DB_PASSWORD": settings.DB_PASSWORD
             }
-
-
