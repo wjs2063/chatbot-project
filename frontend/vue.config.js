01c22374ece46783d4bf637646ccb1123cab9340
wjs2063 <wjs2063@naver.com>
2023-10-21 20:53:23 +0900

    fix: vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,11 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
+    devServer: {
+        port: 50001,
+        allowedHosts:
+            "all"
+    }
+    ,
 })
 
