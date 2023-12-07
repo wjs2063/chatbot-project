5241aa94d1703d75fa11e020b0067d4b0afab791
Jae hyeon <wjs2063@naver.com>
2023-12-07 19:35:00 +0900

    Fix : VideoStreaming muted option remove

--- backend/server/app/api/api_v1/endpoints/streaming.py
+++ backend/server/app/api/api_v1/endpoints/streaming.py
@@ -15,13 +15,18 @@ async def fake_video_streamer():
         yield b"some fake video bytes"
 
 
-# this is streaming test api 
+# this is streaming test api
 @router.get("/")
 async def main():
 
-    some_file_path = "/code/backend/server/Blue_Sky_and_Clouds_Timelapse_0892__Videvo.mp4"
+    some_file_path = "/code/backend/server/weather.mp4"
     def iterfile():  #
+        chunk_size = 1024
         with open(some_file_path, mode="rb") as file_like:  #
-            yield from file_like  #
+            while True:
+                chunk = file_like.read(chunk_size)
+                if not chunk:
+                    break
+                yield chunk
 
     return StreamingResponse(iterfile())
--- frontend/src/components/VideoStreaming.vue
+++ frontend/src/components/VideoStreaming.vue
@@ -7,7 +7,7 @@
     <title>FastAPI video streaming</title>
   </head>
   <body>
-  <video width="1200" controls muted="muted">
+  <video width="1200" controls>
     <source src="http://localhost:50000/api/v1/streaming" type="video/mp4"/>
   </video>
   </body>
