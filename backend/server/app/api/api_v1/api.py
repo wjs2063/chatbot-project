ec81de76e67dd525ba73b51fc424fbfc8bfffcfa
Jae hyeon <wjs2063@naver.com>
2023-12-09 15:27:42 +0900

    delete : 테스트 API 제거

--- backend/server/app/api/api_v1/api.py
+++ backend/server/app/api/api_v1/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from api.api_v1.endpoints import items,streaming
+from api.api_v1.endpoints import llms,streaming
 
 api_router = APIRouter()
 
-api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(llms.router, prefix="/llms", tags=["llms"])
 api_router.include_router(streaming.router, prefix="/streaming", tags=["streaming"])
--- backend/server/app/api/api_v1/endpoints/streaming.py
+++ backend/server/app/api/api_v1/endpoints/streaming.py
@@ -10,9 +10,6 @@ router = APIRouter()
 
 
 
-async def fake_video_streamer():
-    for i in range(10):
-        yield b"some fake video bytes"
 
 
 # this is streaming test api
@@ -21,10 +18,10 @@ async def main():
 
     some_file_path = "/code/backend/server/weather.mp4"
     def iterfile():  #
-        chunk_size = 1024
-        with open(some_file_path, mode="rb") as file_like:  #
+        chunk_size = 1024 * 32
+        with open(some_file_path, mode="rb") as stream_fd:  #
             while True:
-                chunk = file_like.read(chunk_size)
+                chunk = stream_fd.read(chunk_size)
                 if not chunk:
                     break
                 yield chunk
