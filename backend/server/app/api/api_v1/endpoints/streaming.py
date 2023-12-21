8bc0f9243b0053e4311e0b47fe3a984994d6f06e
Jae hyeon <wjs2063@naver.com>
2023-12-21 18:43:07 +0900

    fix : video streaming fastforward and backward(빨리감기,뒤로가기 기능 추가)

--- backend/server/app/api/api_v1/endpoints/streaming.py
+++ backend/server/app/api/api_v1/endpoints/streaming.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request,Header,HTTPException,status
 from fastapi.responses import StreamingResponse
-
+from pathlib import Path
 
 
 
@@ -14,16 +14,35 @@ router = APIRouter()
 
 # this is streaming test api
 @router.get("/")
-async def main():
+async def streaming(range : str = Header()):
+    video_file_path = Path("/code/backend/server/weather.mp4")
+    file_byte_size = video_file_path.stat().st_size
+    range_parts = range.replace('bytes=', '').split('-')
+    start = int(range_parts[0])
+    end = int(range_parts[1]) if len(range_parts) > 1 and range_parts[1] else file_byte_size - 1
 
-    some_file_path = "/code/backend/server/weather.mp4"
-    def iterfile():  #
-        chunk_size = 1024 * 32
-        with open(some_file_path, mode="rb") as stream_fd:  #
-            while True:
-                chunk = stream_fd.read(chunk_size)
-                if not chunk:
-                    break
-                yield chunk
 
-    return StreamingResponse(iterfile())
+
+    if start >= file_byte_size:
+        raise HTTPException(status_code=status.HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE)
+
+    end = min(end, file_byte_size - 1)
+    content_length = end - start + 1
+
+    def stream_file(start, end):
+        with open(video_file_path, mode="rb") as file_bytes:
+            file_bytes.seek(start)
+            stream_chunk = file_bytes.read(end - start)
+            yield stream_chunk
+
+
+    return StreamingResponse(
+        stream_file(start, end + 1),
+        status_code=status.HTTP_206_PARTIAL_CONTENT,
+        media_type="video/mp4",
+        headers={
+            'Accept-Ranges': 'bytes',
+            'Content-Range': f'bytes {start}-{end}/{file_byte_size}',
+            'Content-Length': str(content_length),
+        }
+    )
